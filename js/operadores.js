9ae89e15244bf49b7c5b014dcb4230b743d07846
mkalamarz1 <mkalamarz@gmail.com>
2021-10-16 20:59:43 -0300

    Estudando Javascript

---
+++ js/operadores.js
@@ -0,0 +1,14 @@
+//Você foi para shooping?
+
+var texto = 'Não'
+
+if (texto === 'Sim') {
+    console.log ('Legal!');
+} else if (texto === 'Pensando') {
+    console.log ('Tudo bem!');
+} else {
+    console.log ('Que pena!');
+}
+
+
+
