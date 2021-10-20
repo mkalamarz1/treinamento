9dcb57606797739825c3d1ea357eae34ba7e3346
mkalamarz1 <mkalamarz@gmail.com>
2021-10-20 18:25:25 -0300

    funções

---
+++ js/funcao.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 5;
+
+var tela = somar(a, b);
+console.log("Resultado: " + tela)
+
+function somar(num1, num2){
+
+    let resultado;
+
+    resultado = num1 + num2;
+
+    return resultado;
+}
